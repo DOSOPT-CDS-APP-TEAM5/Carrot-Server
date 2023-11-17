8331f7e3f40dd29b41914f8fa8bd63e2795b6492
nakyeong <rinarina0429@naver.com>
2023-11-17 22:58:37 +0900

    [INIT] 프로그램 초기 셋팅

---
+++ CarrotServer/src/test/java/CarrotServer/CarrotServerApplicationTests.java
@@ -0,0 +1,13 @@
+package CarrotServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrotServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
