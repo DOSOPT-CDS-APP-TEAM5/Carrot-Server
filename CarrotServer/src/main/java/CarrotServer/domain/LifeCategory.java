dafb08b57827d59803a6fe44a482bf6b5c35ad57
sung-silver <jinna0319@gmail.com>
2023-11-23 22:32:19 +0900

    [FEAT] LifeCategory에 Getter 추가

--- CarrotServer/src/main/java/CarrotServer/domain/LifeCategory.java
+++ CarrotServer/src/main/java/CarrotServer/domain/LifeCategory.java
@@ -1,13 +1,15 @@
 package CarrotServer.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Getter
 public enum LifeCategory {
     DICTIONARY("동네백과"),
     QUESTION("동네질문"),
-    ACCIDENT("동네사건사고"),
+    ACCIDENT("동네 사건사고"),
     INFORMATION("생활정보"),
     HOBBY("취미생활");
 
