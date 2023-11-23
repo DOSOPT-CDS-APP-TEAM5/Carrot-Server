c2f0269d431fa2e3d7c58ab441c49d07844ab860
sung-silver <jinna0319@gmail.com>
2023-11-23 21:32:52 +0900

    [REFACTOR] Tag라는 변수명을 일관성 있게 Category로 변경

---
+++ CarrotServer/src/main/java/CarrotServer/controller/LifeController.java
@@ -0,0 +1,2 @@
+package CarrotServer.controller;public class LifeController {
+}
--- CarrotServer/src/main/java/CarrotServer/controller/response/LifeListResponseDTO.java
+++ CarrotServer/src/main/java/CarrotServer/controller/response/LifeListResponseDTO.java
@@ -1,8 +1,8 @@
 package CarrotServer.controller.response;
 
-public record LifeListResponseDTO(Long lifeId, String lifeTagContent, String lifeTitle, String lifeContent, String contentInformation, int likeCount, int commentCount){
-    public static LifeListResponseDTO of(Long lifeId, String lifeTagContent, String lifeTitle, String lifeContent, String contentInformation, int likeCount, int commentCount){
-        return new LifeListResponseDTO(lifeId, lifeTagContent, lifeTitle, lifeContent, contentInformation, likeCount, commentCount);
+public record LifeListResponseDTO(Long lifeId, String lifeCategoryContent, String lifeTitle, String lifeContent, String contentInformation, int likeCount, int commentCount){
+    public static LifeListResponseDTO of(Long lifeId, String lifeCategoryContent, String lifeTitle, String lifeContent, String contentInformation, int likeCount, int commentCount){
+        return new LifeListResponseDTO(lifeId, lifeCategoryContent, lifeTitle, lifeContent, contentInformation, likeCount, commentCount);
     }
 }
 
