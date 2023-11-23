eb4fe09eb138c9d02eb1aefb830f6a600cac172a
sung-silver <jinna0319@gmail.com>
2023-11-23 17:33:27 +0900

    [FEAT] LifeJpaRepository 내 findByCategory 메서드 용도에 맞추어 함수 파라미터 타입을 LifeCategory로 변경

--- CarrotServer/src/main/java/CarrotServer/repository/LifeJpaRepository.java
+++ CarrotServer/src/main/java/CarrotServer/repository/LifeJpaRepository.java
@@ -1,11 +1,12 @@
 package CarrotServer.repository;
 
+import CarrotServer.domain.LifeCategory;
 import CarrotServer.domain.Lives;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LifeJpaRepository extends JpaRepository<Lives, Long>{
-    List<Lives> findAllByLifeCategory(String lifeCategory);
+    List<Lives> findAllByLifeCategory(LifeCategory lifeCategory);
     List<Lives> findAll();
 }
