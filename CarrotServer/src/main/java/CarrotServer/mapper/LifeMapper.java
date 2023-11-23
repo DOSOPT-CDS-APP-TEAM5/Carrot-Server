9169d7571500aa2b755c263a31475c8629f100ff
sung-silver <jinna0319@gmail.com>
2023-11-23 17:32:43 +0900

    [FEAT] LifeMapper 코드 작성

--- CarrotServer/src/main/java/CarrotServer/mapper/LifeMapper.java
+++ CarrotServer/src/main/java/CarrotServer/mapper/LifeMapper.java
@@ -1,5 +1,16 @@
 package CarrotServer.mapper;
 
-@Mapper
+import CarrotServer.controller.response.LifeListResponseDTO;
+import CarrotServer.domain.Lives;
+import org.mapstruct.Mapper;
+import org.mapstruct.ReportingPolicy;
+import org.mapstruct.factory.Mappers;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface LifeMapper {
+    LifeMapper INSTANCE = Mappers.getMapper(LifeMapper.class);
+
+    List<LifeListResponseDTO> toLifeListResponseDTOList(List<Lives> lifeList);
 }
