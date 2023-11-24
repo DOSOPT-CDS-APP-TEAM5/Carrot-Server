14bc3b9967d5d23a1b095238f743c71dee8fbd5a
sung-silver <jinna0319@gmail.com>
2023-11-24 21:04:04 +0900

    [FIX] conflict 해결

--- CarrotServer/src/main/java/CarrotServer/domain/Profiles.java
+++ CarrotServer/src/main/java/CarrotServer/domain/Profiles.java
@@ -1,8 +1,14 @@
 package CarrotServer.domain;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profiles {
     @Id @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long profileId;
@@ -11,4 +17,11 @@ public class Profiles {
     private Clubs club;
     private String nickname;
     private String information;
+
+    @Builder
+    public Profiles(Clubs club, String nickname, String information) {
+        this.club = club;
+        this.nickname = nickname;
+        this.information = information;
+    }
 }
--- CarrotServer/src/main/java/CarrotServer/exception/Success.java
+++ CarrotServer/src/main/java/CarrotServer/exception/Success.java
@@ -12,9 +12,10 @@ public enum Success {
     GET_CLUB_SUCCESS(HttpStatus.OK, "우리동네 모임 상세 조회 성공"),
 
     GET_TOWN_LIVES_SUCCESS(HttpStatus.OK, "동네 생활 리스트 조회 성공"),
-    GET_FILTERED_TOWN_LIVES_SUCCESS(HttpStatus.OK, "카테고리 별 동네 생활 리스트 조회 성공");
+    GET_FILTERED_TOWN_LIVES_SUCCESS(HttpStatus.OK, "카테고리 별 동네 생활 리스트 조회 성공"),
 
     // 201 craeted
+    CREATE_PROFILE_SUCCESS(HttpStatus.CREATED, "모임 프로필 생성 성공");
 
 
     private final HttpStatus httpStatus;
