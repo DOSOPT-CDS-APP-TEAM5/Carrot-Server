f229221e36dd98b063154410181fafc289da3c22
sung-silver <jinna0319@gmail.com>
2023-11-24 21:47:12 +0900

    [FEAT] 동네 모임 리스트 조회에서 사용될 Success 추가

--- CarrotServer/src/main/java/CarrotServer/exception/Success.java
+++ CarrotServer/src/main/java/CarrotServer/exception/Success.java
@@ -12,6 +12,8 @@ public enum Success {
     GET_CLUB_SUCCESS(HttpStatus.OK, "우리동네 모임 상세 조회 성공"),
     GET_TOWN_LIVES_SUCCESS(HttpStatus.OK, "동네 생활 리스트 조회 성공"),
     GET_FILTERED_TOWN_LIVES_SUCCESS(HttpStatus.OK, "카테고리 별 동네 생활 리스트 조회 성공"),
+    GET_TOWN_CLUBS_SUCCESS(HttpStatus.OK, "동네 모임 리스트 조회 성공"),
+    GET_FILTERED_TOWN_CLUBS_SUCCESS(HttpStatus.OK, "카테고리 별 동네 모임 리스트 조회 성공"),
 
     // 201 craeted
     CREATE_PROFILE_SUCCESS(HttpStatus.CREATED, "모임 프로필 생성 성공");
