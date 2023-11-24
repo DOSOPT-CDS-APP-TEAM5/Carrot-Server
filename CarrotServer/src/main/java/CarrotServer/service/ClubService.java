64dc67fff0cb5d2d2fc46d5df545b3bb09d1ac45
sung-silver <jinna0319@gmail.com>
2023-11-24 21:39:38 +0900

    [FEAT] ClubService 내 getClubList, getClubListFilteredByClubCategory 로직 구현

--- CarrotServer/src/main/java/CarrotServer/service/ClubService.java
+++ CarrotServer/src/main/java/CarrotServer/service/ClubService.java
@@ -1,10 +1,16 @@
 package CarrotServer.service;
 
+import CarrotServer.common.enums.ClubCategory;
 import CarrotServer.controller.response.ClubGetResponse;
+import CarrotServer.controller.response.ClubListResponseDTO;
+import CarrotServer.domain.Clubs;
+import CarrotServer.mapper.ClubMapper;
 import CarrotServer.repository.ClubJpaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,5 +20,15 @@ public class ClubService {
 
     public ClubGetResponse getClubById(Long id) {
         return ClubGetResponse.of(clubJpaRepository.findByIdOrThrow(id));
+    }
+
+    public List<ClubListResponseDTO> getClubList() {
+        List<Clubs> clubList = clubJpaRepository.findAll();
+        return ClubMapper.INSTANCE.toClubListResponseDTOList(clubList);
+    }
+
+    public List<ClubListResponseDTO> getClubListFilteredByClubCategory(ClubCategory clubCategory) {
+        List<Clubs> clubList = clubJpaRepository.findAllByClubCategory(clubCategory);
+        return ClubMapper.INSTANCE.toClubListResponseDTOList(clubList);
     }
 }
